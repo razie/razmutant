a24a0aaa9348b484692e63ac7d38a409003aa376
razie <razie@razie>
2009-12-03 18:49:53 +0000

    rewrite in scala - fixing svn issues

--- src/com/razie/assets/StreamInventory.java
+++ src/com/razie/assets/StreamInventory.java
@@ -1,37 +1,33 @@
 package com.razie.assets;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.w3c.dom.Element;
 
+import razie.assets.AssetBrief;
+import razie.assets.AssetImpl;
+import razie.assets.AssetKey;
+import razie.assets.AssetLocation;
+import razie.assets.AssetMap;
+import razie.assets.FileAssetBrief;
+import razie.assets.FileAssetBriefImpl;
+
 import com.razie.dist.db.AgentDb;
-import com.razie.media.ToDeleteMediaInventory;
-import com.razie.pub.assets.AssetBrief;
-import com.razie.pub.assets.AssetKey;
-import com.razie.pub.assets.AssetLocation;
-import com.razie.pub.base.data.HttpUtils;
-import com.razie.pub.base.log.Log;
-import com.razie.pub.media.players.PlayerHandle;
-import com.razie.pub.media.players.PlayerRegistry;
-import com.razie.pub.media.players.SdkPlayer;
+import com.razie.media.ScalaMediaInventoryBik;
 
 /**
  * internet streams - are currently only obtained from user.xml
  * 
  */
-public class StreamInventory extends ToDeleteMediaInventory {
+public class StreamInventory extends ScalaMediaInventoryBik {
     public static final String sCLASS_STREAM = "Stream";
 
-    public Map<AssetKey, AssetBrief> find(String type, AssetLocation env, boolean recurse) {
-        Map<AssetKey, AssetBrief> ret = new HashMap<AssetKey, AssetBrief>();
+    public AssetMap queryAll(String type, AssetLocation env, boolean recurse, AssetMap ret) {
 
         for (Element stream : AgentDb.db("streams").xml().listEntities("/db/streams/stream")) {
-            AssetBrief b = new AssetBrief();
+            FileAssetBriefImpl b = new FileAssetBriefImpl();
 
             String url = stream.getAttribute("url");
             b.setKey(new AssetKey(sCLASS_STREAM, stream.getAttribute("name"), new AssetLocation(url)));
-            b.player = stream.getAttribute("player");
+            b.setPlayer ( stream.getAttribute("player"));
             b.setFileName("");
             b.setLocalDir(url);
             b.setIcon(stream.getAttribute("icon"));
@@ -47,15 +43,15 @@ public class StreamInventory extends ToDeleteMediaInventory {
     @Override
     public AssetBrief getBrief(AssetKey ref) {
         AssetBrief b = super.getBrief(ref);
-        b.setFileName("");
-        b.setLocalDir(ref.getLocation().toHttp());
+        ((FileAssetBrief)b).setFileName("");
+        ((FileAssetBrief)b).setLocalDir(ref.getLocation().toHttp());
         b.setIcon("/mutant/pics/bb_mov_.png");
         return b;
     }
 
     @Override
-    public Object playAsset(String prefPlayerNm, AssetKey ref) {
-       return ToDeleteMediaInventory.simplePlayAsset(prefPlayerNm, ref);
+    public Object playRef(String prefPlayerNm, AssetKey ref) {
+       return playAsset("internet", new AssetImpl(getBrief(ref)));
     }
 
 }
