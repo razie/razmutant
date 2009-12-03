ba0efe5f46156645365c829ffffb926d06576cdf
razie <razie@razie>
2009-12-03 18:49:41 +0000

    rewrite in scala - fixing svn issues

--- src/com/razie/assets/xml/XmlAssetInventory.java
+++ src/com/razie/assets/xml/XmlAssetInventory.java
@@ -1,28 +1,27 @@
 package com.razie.assets.xml;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.w3c.dom.Element;
 
-import com.razie.assets.AssetImpl;
-import com.razie.assets.AssetInventory;
-import com.razie.assets.InventoryAssetMgr;
+import razie.assets.AllOfType;
+import razie.assets.AssetBrief;
+import razie.assets.AssetBrief$;
+import razie.assets.AssetBriefImpl;
+import razie.assets.AssetImpl;
+import razie.assets.AssetInventory;
+import razie.assets.AssetKey;
+import razie.assets.AssetLocation;
+import razie.assets.AssetMap;
+import razie.assets.FileAssetBrief;
+import razie.assets.Meta;
+import razie.assets.QueryCriteria;
+
 import com.razie.dist.db.AgentDb;
-import com.razie.media.ToDeleteMediaInventory;
-import com.razie.pub.assets.AssetBrief;
-import com.razie.pub.assets.AssetKey;
-import com.razie.pub.assets.AssetLocation;
-import com.razie.pub.assets.Meta;
+import com.razie.media.ScalaMediaInventoryBik;
 import com.razie.pub.base.ActionItem;
 import com.razie.pub.base.ScriptContext;
-import com.razie.pub.base.data.HttpUtils;
-import com.razie.pub.base.log.Log;
 import com.razie.pub.draw.Drawable;
-import com.razie.pub.media.players.PlayerHandle;
-import com.razie.pub.media.players.PlayerRegistry;
-import com.razie.pub.media.players.SdkPlayer;
 import com.razie.pub.resources.RazIcons;
 
 /**
@@ -32,9 +31,14 @@ public class XmlAssetInventory implements AssetInventory {
    String clsName;
 
    /** use the base and add details */
-   public Map<AssetKey, AssetBrief> find(String type, AssetLocation env, boolean recurse) {
-      Map<AssetKey, AssetBrief> ret = new HashMap<AssetKey, AssetBrief>();
-   
+   public AssetMap query(QueryCriteria c, AssetLocation env, boolean recurse, AssetMap ret) {
+      AssetMap r = ret;
+     if (c instanceof AllOfType) {
+        r = queryAll (((AllOfType)c).meta(), env, recurse, ret);
+     }
+     return r;
+   }
+   public AssetMap queryAll(String meta, AssetLocation env, boolean recurse, AssetMap ret) {
       for (Element link : AgentDb.db(clsName).xml().listEntities("/db/" + clsName)) {
          AssetBrief b = brief(link);
          ret.put(b.getKey(), b);
@@ -48,11 +52,11 @@ public class XmlAssetInventory implements AssetInventory {
     * @return
     */
    private AssetBrief brief(Element link) {
-      AssetBrief b = new AssetBrief();
+      AssetBriefImpl b = new AssetBriefImpl();
 
-      b.setKey(new AssetKey(clsName, link.getAttribute("key"), new AssetLocation()));
-      b.player = "internet";
-      b.setFileName("");
+      b.setKey(new AssetKey(clsName, link.getAttribute("key")));
+      b.setPlayer ( "internet");
+      ((FileAssetBrief)b).setFileName("");
       //b.setLocalDir(url);
       b.setBriefDesc(link.getAttribute("type"));
       b.setLargeDesc(link.getAttribute("desc"));
@@ -88,13 +92,13 @@ public class XmlAssetInventory implements AssetInventory {
    }
 
    public Object playAsset(String prefPlayerNm, AssetKey ref) {
-      return ToDeleteMediaInventory.simplePlayAsset(prefPlayerNm, ref);
+      return ScalaMediaInventoryBik.playAsset("internet", new AssetImpl(getBrief(ref)));
    }
 
-   private static final ActionItem[] defaultCmds = { AssetBrief.PLAY, AssetBrief.DELETE };
+   private static final ActionItem[] defaultCmds = { AssetBrief$.MODULE$.PLAY(), AssetBrief$.MODULE$.DELETE() };
 
    @Override
-   public Drawable details(AssetBrief asset) {
+   public Drawable getDetails(AssetBrief asset) {
       // TODO Auto-generated method stub
       return null;
    }
@@ -106,7 +110,7 @@ public class XmlAssetInventory implements AssetInventory {
    }
 
    @Override
-   public Object get(AssetKey ref) {
+   public Object getAsset(AssetKey ref) {
       // TODO Auto-generated method stub
       return null;
    }
