ef6156a297d8441a157fe3e19d50b9c27625d184
razie <razie@razie>
2009-12-03 18:50:42 +0000

    rewrite in scala - fixing svn issues

--- src/com/razie/agent/network/NewAgentNetworkService.scala
+++ src/com/razie/agent/network/NewAgentNetworkService.scala
@@ -1,9 +1,9 @@
 package com.razie.agent.network
 
 import com.razie.pub.agent._
-import com.razie.pub.assets.Meta
 import com.razie.assets._
 import com.razie.pub.assets._
+import razie.assets._
 
 class NewAgentNetworkService extends AgentService {
 
