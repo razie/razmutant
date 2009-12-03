50ed48be25e3b7a16303ba5f8c5c5d27733a8bde
razie <razie@razie>
2009-12-03 18:49:46 +0000

    rewrite in scala - fixing svn issues

--- src/com/razie/assets/SiteInventory.java
+++ src/com/razie/assets/SiteInventory.java
@@ -1,33 +1,34 @@
 package com.razie.assets;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.w3c.dom.Element;
 
+import razie.assets.AssetBrief;
+import razie.assets.AssetKey;
+import razie.assets.AssetLocation;
+import razie.assets.AssetMap;
+import razie.assets.FileAssetBrief;
+import razie.assets.FileAssetBriefImpl;
+
 import com.razie.dist.db.AgentDb;
-import com.razie.media.ToDeleteMediaInventory;
-import com.razie.pub.assets.AssetBrief;
-import com.razie.pub.assets.AssetKey;
-import com.razie.pub.assets.AssetLocation;
+import com.razie.media.ScalaMediaInventoryBik;
 import com.razie.pub.comms.Agents;
 
 /**
  * internet streams - are currently only obtained from user.xml
  * 
  */
-public class SiteInventory extends ToDeleteMediaInventory {
+public class SiteInventory extends ScalaMediaInventoryBik {
     public static final String sCLASS_SITE = "Site";
 
-    public Map<AssetKey, AssetBrief> find(String type, AssetLocation env, boolean recurse) {
-        Map<AssetKey, AssetBrief> ret = new HashMap<AssetKey, AssetBrief>();
+    public AssetMap queryAll(String type, AssetLocation env, boolean recurse, AssetMap toUse) {
+        AssetMap ret = new AssetMap();
 
         for (Element stream : AgentDb.db("sites").xml().listEntities("/db/sites/site")) {
-            AssetBrief b = new AssetBrief();
+            FileAssetBriefImpl b = new FileAssetBriefImpl();
 
             String url = stream.getAttribute("url");
             b.setKey(new AssetKey("Site", stream.getAttribute("url"), AssetLocation.mutantEnv(Agents.getMyHostName(), "")));
-            b.player = stream.getAttribute("player");
+            b.setPlayer ( stream.getAttribute("player"));
             b.setFileName("");
             b.setLocalDir(url);
             b.setIcon(stream.getAttribute("icon"));
@@ -43,10 +44,11 @@ public class SiteInventory extends ToDeleteMediaInventory {
 
     public AssetBrief getBrief(AssetKey ref) {
         AssetBrief b = super.getBrief(ref);
+        FileAssetBrief fb = (FileAssetBrief)b;
         b.setIcon("/mutant/pics/bb_mov_.png");
 
-        b.setFileName("");
-        b.setLocalDir(ref.getId());
+        fb.setFileName("");
+        fb.setLocalDir(ref.getId());
         return b;
     }
 }
