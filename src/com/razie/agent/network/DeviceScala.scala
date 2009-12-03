140839c2c6b3a0258725ceec928683263c134aec
razie <razie@razie>
2009-12-03 18:50:32 +0000

    rewrite in scala - fixing svn issues

--- src/com/razie/agent/network/DeviceScala.scala
+++ src/com/razie/agent/network/DeviceScala.scala
@@ -4,6 +4,7 @@ import com.razie.pub.assets._
 import com.razie.agent.network._
 import com.razie.agent.network.Device.Impl
 import com.razie.pub.lightsoa._
+import razie.assets._
 
 @SoaAsset(meta = "Device", descr = "?")
 class DeviceScala (key:AssetKey) extends Impl(key) {
